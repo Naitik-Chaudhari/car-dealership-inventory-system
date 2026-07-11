16736eb5be7c7ac230736dd9dfa13ee3f595e4a4
Naitik-Chaudhari <naitikchaudhari3011@gmail.com>
2026-07-11 14:43:31 +0530

    feat: prevent duplicate user registration

--- backend/src/main/java/com/naitik/car_dealership_api/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/naitik/car_dealership_api/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.naitik.car_dealership_api.service.impl;
 import com.naitik.car_dealership_api.dto.request.RegisterRequest;
 import com.naitik.car_dealership_api.entity.Role;
 import com.naitik.car_dealership_api.entity.User;
+import com.naitik.car_dealership_api.exception.EmailAlreadyExistsException;
 import com.naitik.car_dealership_api.repository.UserRepository;
 import com.naitik.car_dealership_api.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void register(RegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new EmailAlreadyExistsException("Email already registered.");
+        }
+
         User user = User.builder()
                 .name(request.getName())
                 .email(request.getEmail())
